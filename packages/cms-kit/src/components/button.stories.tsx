354ff5c4bddae450cd7196093a314a0a124c6196
Till Hagger <till.hagger@gmail.com>
2019-08-08 20:01:06 +0200

    Remove unused import.

--- packages/cms-kit/src/components/button.stories.tsx
+++ packages/cms-kit/src/components/button.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import {storiesOf} from '@storybook/react'
 import {text} from '@storybook/addon-knobs'
-import {StyleProvider, createStyleRenderer, renderStyles} from '@karma.run/react'
 
 import {Button} from './button'
 
