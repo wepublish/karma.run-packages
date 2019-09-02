2585111774165d909ff355c9d6f69e83195a0041
Till Hagger <till.hagger@gmail.com>
2019-09-02 12:18:21 +0200

    Fix optional CSS font props. Up version.

--- packages/react/src/style.tsx
+++ packages/react/src/style.tsx
@@ -154,11 +154,11 @@ export function cssKeyframes<P = undefined>(keyframesRuleFn: CSSKeyframesFn<P>):
 export interface UseStyleResult<P> {
   staticCSS(selector: string, style: CSSStyle): void
   css(...rules: CSSRuleFn<P>[]): string
-  font(family: string, files: string[], props: FontCSSProps): void
+  font(family: string, files: string[], fontProps?: CSSFontProps): void
   keyframes(keyframesFn: CSSKeyframesFn<P>): string
 }
 
-export interface FontCSSProps {
+export interface CSSFontProps {
   fontDisplay: 'auto' | 'block' | 'swap' | 'fallback' | 'optional'
   fontVariant: string
   fontWeight: string
@@ -188,7 +188,7 @@ export function useStyle<P>(props?: P): UseStyleResult<P> {
       // `as any` is needed because there's no generic version of the rest parameter function overload
       return renderer.renderRule(combineRules(...(rules as any)), props as any)
     },
-    font(family: string, files: string[], fontProps?: FontCSSProps): void {
+    font(family: string, files: string[], fontProps?: CSSFontProps): void {
       renderer.renderFont(family, files, fontProps)
     },
     keyframes(keyframesFn: CSSKeyframesFn<P>): string {
