305733f06dd3e808b74675647d7792b612c72a9b
Gabriel Anliker <gabriel.anliker@fastmail.com>
2019-09-25 18:44:13 +0200

    Feature / Styling FilterTag (#15)

--- packages/cms-kit/src/atoms/filterTag.tsx
+++ packages/cms-kit/src/atoms/filterTag.tsx
@@ -3,18 +3,31 @@ import React from 'react'
 import {IconType, Icon} from './icon'
 import {cssRuleWithTheme, useThemeStyle} from '../style/themeContext'
 import {BaseButton} from './baseButton'
-import {FontSize} from '../style/helpers'
+import {FontSize, pxToRem, Spacing, TransitionDuration} from '../style/helpers'
 
 const FilterTagStyle = cssRuleWithTheme(({theme}) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
   backgroundColor: theme.colors.action,
-  fontSize: FontSize.Medium
+  fontSize: pxToRem(FontSize.Small),
+  minWidth: pxToRem(100),
+  height: pxToRem(30),
+  overflow: 'hidden',
+  borderRadius: '2px',
+  border: `1px solid ${theme.colors.actionDark}`,
+  color: theme.colors.white,
+  lineHeight: pxToRem(30),
+  paddingLeft: pxToRem(Spacing.Tiny)
 }))
 
 const CloseButtonStyle = cssRuleWithTheme(({theme}) => ({
   fill: theme.colors.white,
+  fontSize: pxToRem(FontSize.Medium),
+  transition: `fill ease-in ${TransitionDuration.Fast}`,
+  width: pxToRem(20),
 
   ':hover': {
-    fill: theme.colors.primary
+    fill: theme.colors.actionDark
   }
 }))
 
