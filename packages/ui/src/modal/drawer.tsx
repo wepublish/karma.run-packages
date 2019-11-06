5af6a739db41056836db8f08e2eee087b81f50c4
Till Hagger <till.hagger@gmail.com>
2019-11-06 13:57:04 +0100

    Set background color for Drawer.

--- packages/ui/src/modal/drawer.tsx
+++ packages/ui/src/modal/drawer.tsx
@@ -3,34 +3,42 @@ import {styled} from '@karma.run/react'
 import {TransitionDuration, pxToRem} from '../style/helpers'
 import {TransitionStatus} from 'react-transition-group/Transition'
 import {Modal, ModalProps} from './modal'
+import {themeMiddleware, Theme} from '../style/themeContext'
 
 interface DrawerWrapperProps {
   readonly transitionStatus: TransitionStatus
   readonly width?: number | string
+  readonly theme: Theme
 }
 
-const DrawerWrapper = styled('div', ({transitionStatus, width}: DrawerWrapperProps) => ({
-  _className: process.env.NODE_ENV !== 'production' ? 'Drawer' : undefined,
+const DrawerWrapper = styled(
+  'div',
+  ({transitionStatus, width, theme}: DrawerWrapperProps) => ({
+    _className: process.env.NODE_ENV !== 'production' ? 'Drawer' : undefined,
 
-  position: 'absolute',
-  right: 0,
+    position: 'absolute',
+    right: 0,
 
-  width: typeof width === 'number' ? pxToRem(width) : width,
-  maxHeight: '100vh',
+    width: typeof width === 'number' ? pxToRem(width) : width,
+    height: '100vh',
 
-  overflowX: 'hidden',
-  overflowY: 'auto',
+    overflowX: 'hidden',
+    overflowY: 'auto',
 
-  transitionProperty: 'transform box-shadow',
-  transitionDuration: TransitionDuration.Slow,
+    backgroundColor: theme.colors.white,
 
-  boxShadow:
-    transitionStatus === 'entered'
-      ? '0 0 10px 0 rgba(0, 0, 0, 0.2)'
-      : '0 0 10px 0 rgba(0, 0, 0, 0)',
+    boxShadow:
+      transitionStatus === 'entered'
+        ? '0 0 10px 0 rgba(0, 0, 0, 0.2)'
+        : '0 0 10px 0 rgba(0, 0, 0, 0)',
 
-  transform: transitionStatus === 'entered' ? 'translateX(0)' : 'translateX(100%)'
-}))
+    transform: transitionStatus === 'entered' ? 'translateX(0)' : 'translateX(100%)',
+
+    transitionProperty: 'transform box-shadow',
+    transitionDuration: TransitionDuration.Slow
+  }),
+  themeMiddleware
+)
 
 export interface DrawerProps extends ModalProps {
   readonly width?: number
