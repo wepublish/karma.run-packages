87478f172f2d166873bb3cdd693f0d60a81894be
Till Hagger <till.hagger@gmail.com>
2019-11-06 15:09:51 +0100

    Set FocalPoint background color to black.

--- packages/ui/src/input/focalPointInput.tsx
+++ packages/ui/src/input/focalPointInput.tsx
@@ -96,7 +96,7 @@ export function FocalPointInput({
 const FocalPointStyle = cssRuleWithTheme(({theme}) => ({
   width: pxToRem(50),
   height: pxToRem(50),
-  backgroundColor: 'rgba(255, 255, 255, 0.2)',
+  backgroundColor: 'rgba(0, 0, 0, 0.2)',
 
   borderRadius: '100%',
   border: `1px solid ${theme.colors.white}`,
@@ -109,7 +109,7 @@ const FocalPointStyle = cssRuleWithTheme(({theme}) => ({
   fontSize: pxToRem(FontSize.Heading3),
 
   '&:hover': {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)'
+    backgroundColor: 'rgba(0, 0, 0, 0.4)'
   }
 }))
 
